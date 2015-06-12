600851072d0fc28e784b7c4e033466d376fd6442
Gérald Tibi <gerald_tibi@natural-solutions.eu>
2015-06-12 14:23:35 +0200

    Some corrections

--- ThesaurusEditor/backboneForm-editors-autocompTree.js
+++ ThesaurusEditor/backboneForm-editors-autocompTree.js
@@ -1,80 +1,112 @@
 define([
 	'underscore',
 	'jquery',
+    'jqueryui',
 	'backbone',
-	'backbone-forms',
+	'backbone_forms',
 	'autocompTree',
-	'config',
-], function(
-	_, $, Backbone, Form, autocompTree, config
-){
-	'use strict';
-	return Form.editors.AutocompTreeEditor = Form.editors.Base.extend({
+], function (
+	_, $, $ui, Backbone, Form, autocompTree
+) {
+    'use strict';
+    return Form.editors.AutocompTreeEditor = Form.editors.Base.extend({
 
 
-		previousValue: '',
+        previousValue: '',
 
-		events: {
-			'hide': "hasChanged"
-		},
+        events: {
+            'hide': "hasChanged"
+        },
 
-		hasChanged: function(currentValue) {
-			if (currentValue !== this.previousValue){
-				this.previousValue = currentValue;
-				this.trigger('change', this);
-			}
-		},
+        hasChanged: function (currentValue) {
+            if (currentValue !== this.previousValue) {
+                this.previousValue = currentValue;
+                this.trigger('change', this);
+            }
+        },
 
-		initialize: function(options) {
-			console.log(this) ;
-			console.log(options);
-			Form.editors.Base.prototype.initialize.call(this, options);
-			this.template = options.template || this.constructor.template;
-			this.id = options.id;
-			var tplValeurs = {
-				inputID: this.id
-			}
-			this.template = _.template(this.template,tplValeurs) ;
-			this.startId = options.startId;
-			this.startId = 2887345;
-			console.log(this.startId);
-		},
+        initialize: function (options) {
+            this.languages = {
+                'fr':'',
+                'en':'En'
+            };
+            console.log(this);
+            console.log('*********************** OPTIONS *******************************');
+            console.log(options);
+            Form.editors.Base.prototype.initialize.call(this, options);
+            this.template = options.template || this.constructor.template;
+            this.id = options.id;
+            var editorAttrs = "";
+            if (options.schema.editorAttrs && options.schema.editorAttrs.disabled) {
 
-		getValue: function() {
-			var date= new Date
-			return this.$el.find('#' + this.id ).val() ;
-		},
+                editorAttrs += 'disabled="disabled"';
+            }
+            var tplValeurs = {
+                inputID: this.id,
+                editorAttrs: editorAttrs,
+                editorClass: options.schema.editorClass
+            }
+            this.template = _.template(this.template, tplValeurs);
+            this.startId = options.schema.options.startId;
+            this.wsUrl = options.schema.options.wsUrl;
+            this.lng = options.schema.options.lng;
+            this.displayValueName = options.displayValueName || 'fullpathTranslated';
+            this.storedValueName = options.storedValueName || 'fullpath';
+        },
 
-		render: function(){
+        getValue: function () {
+            var date = new Date
+            return this.$el.find('#' + this.id + '_value').val();
+        },
 
-			var $el = $(this.template);
-			this.setElement($el);
+        render: function () {
+
+            var $el = $(this.template);
+            this.setElement($el);
 
 
-			var _this= this;
+            var _this = this;
+            _(function () {
+                _this.$el.find('#' + _this.id).autocompTree({
+                    wsUrl: _this.wsUrl + '/ThesaurusREADServices.svc/json',
+                    webservices: 'fastInitForCompleteTree',
+                    language: { hasLanguage: true, lng: _this.lng },
+                    display: {
+                        isDisplayDifferent: true,
+                        suffixeId: '_value',
+                        displayValueName: _this.displayValueName,
+                        storedValueName: _this.storedValueName
+                    },
+                    inputValue: _this.value,
+                    startId: _this.startId
+                });
+            }).defer();
+            var TypeField = "FullPath";
+            if (_this.value && _this.value.indexOf(">") == -1) {
+                TypeField = 'Name';
+            }
+            $.ajax({
+                url: _this.wsUrl + "/ThesaurusReadServices.svc/json/getTRaductionByType",
+                timeout: 10000,
+                data: '{ "sInfo" : "' + _this.value || '' + '", "sTypeField" : "' + TypeField + '", "iParentId":"' + _this.startId + '" }',
+                dataType: "json",
+                type: "POST",
+                contentType: "application/json; charset=utf-8",
+                success: function (data) {
+                    
+                    var translatedValue = data["TTop_FullPath" + _this.languages[_this.lng.toLowerCase()]];
+                    if (TypeField == 'Name') {
+                        translatedValue = data["TTop_Name" + _this.languages[_this.lng.toLowerCase()]];
+                    }
+                    _this.$el.find('#' + _this.id).val(translatedValue);
+                }
+            });
+            return this;
+        },
 
-
-			_(function() {
-				_this.$el.find('#' + _this.id ).autocompTree({
-					wsUrl: config.serverUrl+'/ThesaurusREADServices.svc/json',
-					webservices: 'fastInitForCompleteTree',  
-					language: {hasLanguage:true, lng:"en"},
-					display: {
-						isDisplayDifferent: true,
-						suffixeId: '_value',
-						displayValueName: 'value',
-						storedValueName: 'fullpathTranslated'
-					},
-					startId: _this.startId
-				});
-			}).defer();
-
-			return this;
-		},
-
-	},{
-		template: '<div><input id="<%=inputID%>" name="<%=inputID%>" class="form-control autocompTree" type="text" placeholder="" ></div>',
-	});
+    }, {
+        template: '<div><input id="<%=inputID%>" name="<%=inputID%>" class="autocompTree <%=editorClass%>" type="text" placeholder="" <%=editorAttrs%>></div>',
+    });
 
 
 });
